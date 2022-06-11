8f86df1b2abef8c959cf1cb8bb0b3c6705c66206
ShotaKitazawa <skitazawa1121@gmail.com>
2022-06-11 14:09:11 +0900

    add feature of targetStrip

--- pkg/etcd/etcd.go
+++ pkg/etcd/etcd.go
@@ -3,11 +3,13 @@ package etcd
 import (
 	"context"
 	"crypto/tls"
+	"encoding/json"
 	"time"
 
 	clientv3 "go.etcd.io/etcd/client/v3"
 
 	"github.com/ShotaKitazawa/record-syncer/pkg/config"
+	"github.com/creasty/defaults"
 )
 
 func Watch(ctx context.Context, conf config.Config) (clientv3.WatchChan, error) {
@@ -29,4 +31,21 @@ func Watch(ctx context.Context, conf config.Config) (clientv3.WatchChan, error) 
 		return nil, err
 	}
 	return cli.Watch(ctx, conf.BasePath, clientv3.WithPrefix()), nil
+}
+
+type CoreDnsRecord struct {
+	Host        string `json:"host,omitempty"`
+	TTL         int64  `json:"ttl,omitempty" default:"60"`
+	TargetStrip int    `json:"target_strip,omitempty"`
+}
+
+func MarshalRecord(b []byte) (*CoreDnsRecord, error) {
+	record := &CoreDnsRecord{}
+	if err := json.Unmarshal(b, record); err != nil {
+		return nil, err
+	}
+	if err := defaults.Set(record); err != nil {
+		return nil, err
+	}
+	return record, nil
 }
